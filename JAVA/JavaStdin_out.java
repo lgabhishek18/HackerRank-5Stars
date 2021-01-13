4e60a6bec659cbfd7477db44647c5ed1a65fcc25
Abhishek Gupta <60277134+lgabhishek18@users.noreply.github.com>
2021-01-14 01:58:31 +0530

    Create JavaStdin_out.java

---
+++ JAVA/JavaStdin_out.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution
+{
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a,b,c;
+        a=s.nextInt();
+        b=s.nextInt();
+        c=s.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
