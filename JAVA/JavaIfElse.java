4dcf94006c018bf6a356e28155a27727e390c490
Abhishek Gupta <60277134+lgabhishek18@users.noreply.github.com>
2021-01-15 11:20:59 +0530

    Create JavaIfElse.java

---
+++ JAVA/JavaIfElse.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+//    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int n; 
+        n=s.nextInt();
+    
+        if(n%2!=0)
+        {
+            System.out.println("Weird");
+        }
+        else if(n%2==0)
+        {
+            if(n>=2 && n<=5)
+                System.out.println("Not Weird");
+            if(n>=6 && n<=20)
+                System.out.println("Weird");
+            else if(n>20)
+                System.out.println("Not Weird");
+
+        }
+    }
+}
