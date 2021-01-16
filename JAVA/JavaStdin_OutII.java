39367939d01524a98c909587a3f586ae157d0b04
Abhishek Gupta <60277134+lgabhishek18@users.noreply.github.com>
2021-01-17 02:30:57 +0530

    Create JavaStdin_OutII.java

---
+++ JAVA/JavaStdin_OutII.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int a;
+        double b;
+        String c;
+        a=s.nextInt();
+        b=s.nextDouble();
+        s.nextLine();
+        c=s.nextLine();
+        
+
+        // Write your code here.
+
+        System.out.println("String: " + c);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+    }
+}
+
